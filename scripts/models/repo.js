d1e077b3e632b45016886e975742f8bbd688d51d
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-05 13:36:26 -0700

    Refactored request into .ajax call

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,12 +4,15 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    // TODO: refactor this request into an $.ajax call
-    $.get('/github/users/codefellows-portland-301d6/repos' +
-          '?per_page=10&sort=updated')
-          .done(function(data) {
-            reposObj.allRepos = data;
-          }).done(callback);
+    // TODO/DONE: refactor this request into an $.ajax call
+    $.ajax({
+      url: '/github/users/codefellows-portland-301d6/repos' +
+          '?per_page=10&sort=updated',
+      success: function(data) {
+        reposObj.allRepos = data;
+        callback();
+      }
+    });
   };
 
   reposObj.withTheAttribute = function(attr) {
